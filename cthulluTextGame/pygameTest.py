8cce5f8431292f70c961f370e8763652b930cccb
Terrien <45405194+TerrienDJV5@users.noreply.github.com>
2021-04-26 11:03:17 -0400

    Update pygameTest.py

--- cthulluTextGame/pygameTest.py
+++ cthulluTextGame/pygameTest.py
@@ -22,23 +22,27 @@ display_surface = pygame.display.set_mode((X, Y))
 pygame.display.set_caption('Show Text')
 font = pygame.font.Font('freesansbold.ttf', 32)
 
-for line in charList:
 
 
+def print2screen(charList):
+    vartop = 0
+    for line in charList:
+        text = font.render(line, True, blue, white)
+        textRect = text.get_rect()
+        textRect.top = vartop
+        vartop += 40
+        textRect.left = 20
+        display_surface.blit(text, textRect)
+        
+    
 
-text1 = font.render('apple apple', True, blue, white)
-textRect1 = text1.get_rect()
-textRect1.left = 20
-text2 = font.render('banana', True, blue, white)
-textRect2 = text2.get_rect()
-textRect2.top = 40
-textRect2.left = 20
+
 
 # infinite loop
 while True:
     display_surface.fill(white)
-    display_surface.blit(text1, textRect1)
-    display_surface.blit(text2, textRect2)
+    print2screen(charList)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
